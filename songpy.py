f115f8aa1b0b03b6a334e177c26352130858b060
Lokesh Sharma <lokeshhsharma@gmail.com>
2016-09-21 22:12:47 +0530

    add video support

--- songpy.py
+++ songpy.py
@@ -2,7 +2,11 @@ from google import search
 from os import system
 
 song_name = raw_input('Enter song name: ')
+video = raw_input('Do you want video? (y/n): ') == 'y'
 result = search(song_name).next()
-system('youtube-dl -f bestaudio ' + result)
+if video:
+	system('youtube-dl ' + result)
+else:
+	system('youtube-dl -f bestaudio ' + result)
 
 
